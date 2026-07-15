dd6a575c32a2e47baef9291675c42ec1d87c6a8c
Omar-Elhorbity <omarhusseinelhorbity@gmail.com>
2026-07-15 09:35:49 +0300

    email normalization and no email error

--- core/models.py
+++ core/models.py
@@ -8,7 +8,10 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        normalized_email = self.normalize_email(email)
+        user = self.model(email=normalized_email, **extra_fields) 
         user.set_password(password)
         user.save(using=self._db)
 
--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -5,12 +5,32 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
-        passowrd = "password123"
+        password = "password123"
 
         user = get_user_model().objects.create_user(
             email=email,
-            passowrd=passowrd
+            password=password
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(passowrd))
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email,
+                password="sample123"
+            )
+
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
